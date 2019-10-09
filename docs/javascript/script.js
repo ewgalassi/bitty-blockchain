86caa26c1be9506e6c89b6ec8b6fc7ff73bb8fc2
egalassi <eric.galassi@ni.com>
2019-10-09 10:51:11 -0500

    asdfads

--- docs/javascript/script.js
+++ docs/javascript/script.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
-    console.log("ready")
-    $.get("/", 
-        function (data) {
+    var fs = require("fs");
+    fs.readFile("blocks.txt", 
+        function (err, data) {
             data.map(function (trxn) {
                 var td0 = $("<td>").text(trxn.index).attr("class", "cell");
                 var td1 = $("<td>").text(trxn.previousHash).attr("class", "cell");
@@ -18,129 +18,25 @@ $(document).ready(function () {
         event.preventDefault();
         var username = $("#customer").val().trim();
         var customer = {
-            username: username
+            username: username,
+            quantityOwned: 0
         };
-
-        $.post("/api/customer" + username, 
-            function () {
-                console.log("Created user", username);
+        fs.readFile("customers.txt", function (err, data) {
+            if (!data.find(x => x.username === username)) {
+                fs.appendFile("customers.txt", customer, 
+                    function (err) {
+                        if (err) throw err;
+                        console.log("Created user", username);
+                });
+            } else {
+                var idx = data.findIndex(x => x.username === username)
+                customer = data[idx]
             }
-        );
-        $("#customerInfo").text("Hello, " + username + "! You currently have 0 PPcOIns").css("font-size", "24px").css("text-align", "right").css("padding-right", "150px");
+        });
+        $("#customerInfo").text("Hello, " + username + "! You currently have " + customer.quantityOwned + " PPcOIns").css("font-size", "24px").css("text-align", "right").css("padding-right", "150px");
 
         window.localStorage.setItem("username", username);
         $("#customer").style("visibility", "hidden");
         $("#transaction").style("visibility", "visible");
     });
 });
-// const $characterStock = $("#character-stock");
-// const $customerButton = $("#customerButton");
-// const $blockTable = $("#blockTable");
-// var $customerInfo = $("#customerInfo");
-// var $customer = $("#customer");
-// var customer;
-// var opponentId;
-// var youId;
-// var charId;
-
-// var getBlockData = function () {
-//     $.get("blockchain.accdb").then(function() {
-//         refreshBlocks();
-//     });
-// };
-
-// var customerCreator = function (event) {
-//     event.preventDefault();
-//     var username = $customer.val().trim();
-
-//     API.getCustomer().then(function (data) {
-//         customer = {
-//             username: username,
-//             quantityOwned: data.quantityOwned
-//         };
-//     // })
-//     // $.get("/api/customer").then(function (data) {
-
-//     });
-
-//     window.localStorage.setItem("username", username);
-//     window.localStorage.setItem("quantityOwned", "0");
-
-//     $customerInfo.text("Hello, " + username + "!  You have 0 PPcOIns currently");
-// };
-
-// // copied functions *************************************************************** //
-
-// var API = {
-//     saveBlock: function (block) {
-//         return $.ajax({
-//             headers: {
-//                 "Content-Type": "application/json"
-//             },
-//             type: "POST",
-//             url: "blockchain.accdb",
-//             data: JSON.stringify(block)
-//         });
-//     },
-//     getBlocks: function () {
-//         return $.ajax({
-//             url: "blockchain.accdb",
-//             type: "GET"
-//         });
-//     },
-//     saveCustomer: function (customer) {
-//         return $.ajax({
-//             headers: {
-//                 "Content-Type": "application/json"
-//             },
-//             type: "POST",
-//             url: "blockchain.accdb",
-//             data: JSON.stringify(customer)
-//         });
-//     },
-//     getCustomer: function () {
-//             return $.ajax({
-//             url: "blockchain.accdb",
-//             type: "GET"
-//         });
-//     },
-//     updateCustomer: function (username) {
-//         return $.ajax({
-//             url: "blockchain.accdb",
-//             type: "PUT"
-//         });
-//     }
-// };
-
-// // refreshBlocks gets new blocks from the db and repopulates the list
-// var refreshBlocks = function () {
-//     API.getBlocks().then(function (data) {
-//         // console.log(data);
-
-//         var $block = data.map(function (block) {
-
-//             var $tr = $("<tr>")
-//                 .append($("<td>").text(block.index))
-//                 .append($("<td>").text(block.previousHash))
-//                 .append($("<td>").text(block.timestamp))
-//                 .append($("<td>").text(block.transaction))
-//                 .append($("<td>").text(block.hash));
-
-//             return $tr;
-
-//         });
-
-//         $blockTable.empty();
-//         $blockTable.unshift($block);
-//     });
-// };
-
-// // HANDLERS ***********************************************************
-
-// $customerButton.on("click", customerCreator);
-
-// $(document).ready(function () {
-
-//     getBlockData();
-   
-// });
